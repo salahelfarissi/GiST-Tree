03bf9ccef0de4a4b00278720948ff59beb90afe0
salah.elfarissi <salaheddineelfarissi@gmail.com>
2021-04-15 11:03:31 +0000

    Redone process

--- bds_projet.sql
+++ bds_projet.sql
@@ -8,32 +8,16 @@ UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'postgis';
 create database mono template postgis;
 ALTER DATABASE mono SET search_path='$user', public, postgis;
 --
-shp2pgsql -s 26191 -g geom -I .\communes.shp communes | psql -U elfarissi -d mono
---
-vacuum analyse communes;
---
-select commune from communes
-where commune like'Agadir%' or commune like 'Taroudannt%' or commune like 'Ait Baha%' 
-or commune like 'Tata %' or commune like 'Tiznit%';
---
-drop table communes;
---
 select srid, proj4text from spatial_ref_sys where srtext like '%WGS_1984%';
 --
+shp2pgsql -s 4326 -g geom -I .\regions.shp regions | psql -U elfarissi -d mono
 shp2pgsql -s 4326 -g geom -I .\provinces.shp provinces | psql -U elfarissi -d mono
--- souss_massa table created
-select nom_provin as "name", st_transform(geom, 26192) as geom 
-into souss_massa
-from provinces
-where nom_provin in ('AGADIR IDA OU TANAN', 'INEZGANE AIT MELLOUL', 'TAROUDANNT', 'CHTOUKA AIT BAHA', 'TATA', 'TIZNIT');
+shp2pgsql -s 4326 -g geom -I .\communes.shp communes | psql -U elfarissi -d mono
+-- update statistics
+vacuum analyze regions, provinces, communes;
 --
-select st_srid(geom) from souss_massa limit 1;
+ALTER TABLE regions
+RENAME COLUMN code_regio TO r_code;
 --
-select max(length(name)) from souss_massa;
---
-alter table souss_massa
-alter column "name" type varchar(20);
---
-update souss_massa
-set "name" = 'AGADIR IDA OU TANANE'
-where "name" = 'AGADIR IDA OU TANAN';
+ALTER TABLE regions
+RENAME COLUMN nom_region TO r_nom;
