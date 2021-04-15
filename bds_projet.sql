c4e13b7484c98d69feead1daa5386418aad6e0e1
salah.elfarissi <salaheddineelfarissi@gmail.com>
2021-04-15 07:22:41 +0000

    database creation

---
+++ bds_projet.sql
@@ -0,0 +1,9 @@
+create database postgis;
+-- \c postgis
+create SCHEMA postgis;
+CREATE EXTENSION postgis SCHEMA postgis;
+-- setting postgis db as template
+UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'postgis';
+-- 
+create database mono template postgis;
+ALTER DATABASE mono SET search_path='$user', public, postgis;
