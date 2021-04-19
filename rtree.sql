9581863990bac0d9432f70a83b48dceb70d4514f
salah.elfarissi <salaheddineelfarissi@gmail.com>
2021-04-19 19:01:26 +0000

    polygon to linestring

--- rtree.sql
+++ rtree.sql
@@ -6,3 +6,6 @@ CREATE TABLE rtree (geom geometry);
 INSERT INTO rtree
 SELECT replace(a::text, '2DF', '')::box2d::geometry as geom
 FROM (SELECT * FROM gist_print('29333') as t(level int, valid bool, a box2df) WHERE level =1) AS subq;
+
+--polygon to linestring
+select st_astext(st_exteriorring(geom)) from rtree_v2;
