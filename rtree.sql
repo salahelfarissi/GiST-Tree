d7a5c89ca6b24ecea80a2544b65287a58e6c82ea
salah.elfarissi <salaheddineelfarissi@gmail.com>
2021-04-20 08:32:04 +0000

    Deleted code

--- rtree.sql
+++ rtree.sql
@@ -26,6 +26,3 @@ CREATE TABLE rtree (geom geometry);
 INSERT INTO rtree
 SELECT replace(a::text, '2DF', '')::box2d::geometry as geom
 FROM (SELECT * FROM gist_print('29333') as t(level int, valid bool, a box2df) WHERE level =1) AS subq;
-
--- Polygon to linestring
-SELECT st_astext(st_exteriorring(geom)) FROM rtree_v2;
