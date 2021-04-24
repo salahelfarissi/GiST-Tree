15e38a6e9e4729a16ee02a096e53e343f0de4251
salah.elfarissi <salaheddineelfarissi@gmail.com>
2021-04-24 06:04:17 +0000

    rtree → r-tree

---
+++ r-tree.sql
@@ -0,0 +1,28 @@
+-- Retrieve OIDs of a spatial indices
+WITH nom_table AS (
+        SELECT f_table_name AS nom_table
+        FROM geometry_columns
+    )
+SELECT
+    relname,
+    CAST(c.oid AS INTEGER) FROM pg_class c, pg_index i  
+WHERE c.oid = i.indexrelid and c.relname IN (
+    WITH nom_table AS (
+        SELECT f_table_name AS nom_table
+        FROM geometry_columns
+        )
+    SELECT
+        relname
+    FROM pg_class, pg_index
+    WHERE pg_class.oid = pg_index.indexrelid
+    AND pg_class.oid IN (
+        SELECT indexrelid FROM pg_index, pg_class
+        WHERE pg_class.relname IN (SELECT nom_table FROM nom_table)
+        AND pg_class.oid=pg_index.indrelid
+        AND indisunique != 't'
+        AND indisprimary != 't' )
+);
+
+-- Viz
+SELECT replace(a::text, '2DF', '')::box2d::geometry as geom
+FROM (SELECT * FROM gist_print('29415') as t(level int, valid bool, a box2df) WHERE level =1) AS subq;
