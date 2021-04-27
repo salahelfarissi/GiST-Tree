36223e54ddaddebac75b52c069859f8557c67675
salah.elfarissi <salaheddineelfarissi@gmail.com>
2021-04-27 06:35:46 +0000

    csv

--- data.py
+++ data.py
@@ -1,10 +1,5 @@
 import pandas as pd
 
 df = pd.read_csv('tree.csv')
-df.drop('col8', inplace=True, axis=1)
-df.drop('col9', inplace=True, axis=1)
-df.drop('col2', inplace=True, axis=1)
-df.drop('col4', inplace=True, axis=1)
-df.drop('col6', inplace=True, axis=1)
 
-print(df)
+df.to_csv('tree.csv')
