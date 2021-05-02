e2470c9aed671cbe4a4700b8bc33ed58225f78a4
salahelfarissi <salaheddineelfarissi@gmail.com>
2021-05-02 11:13:43 +0000

    Hope

--- cascade.py
+++ cascade.py
@@ -1,56 +1,49 @@
 import psycopg2
 
-# Connect to db
+# * Connect to an existing database
+# ! Host ip changes for virtual machines
 conn = psycopg2.connect("""
-    host=192.168.1.104
+    host=192.168.1.107
     dbname=mono
-    user=elfarissi
     password='%D2a3#PsT'
     """)
 
-# Cursor
+# * Open a cursor to perform database operations
 cur = conn.cursor()
 
-# com_cas (communes cascade) is the table where features will be inserted
-cur.execute("""DROP TABLE IF EXISTS cascade.com_cas;
-    """)
+cur.execute("CREATE SCHEMA IF NOT EXISTS cascade;")
 
-cur.execute("""CREATE TABLE cascade.com_cas (
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS cascade.com_cas (
         c_code varchar(32),
-        geom geometry(MultiPolygon, 4326));
-    """)
+        geom geometry(MultiPolygon, 4326));""")
 
-# Create index using GiST
-cur.execute("""CREATE INDEX com_cas_geom_idx
-	ON cascade.com_cas USING gist
-	(geom);
-    """)
+cur.execute("TRUNCATE TABLE cascade.com_cas;")
 
-# r_tree will be updated as soon as new features get added
-cur.execute("""DROP TABLE IF EXISTS cascade.r_tree;
-    """)
+cur.execute("""
+    CREATE INDEX IF NOT EXISTS com_cas_geom_idx
+        ON cascade.com_cas USING gist
+        (geom);""")
 
-# indices stores identifiers
-cur.execute("DROP TABLE IF EXISTS cascade.indices;")
-cur.execute("""CREATE TABLE cascade.indices (
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS cascade.indices (
     idx_oid serial primary key, 
     idx_name varchar);
     """)
 
-cur.execute("""INSERT INTO cascade.indices
+cur.execute("TRUNCATE TABLE cascade.indices;")
 
+cur.execute("""INSERT INTO cascade.indices
     WITH gt_name AS (
         SELECT
             f_table_name AS t_name
         FROM geometry_columns
     )
-
     SELECT
         CAST(c.oid AS INTEGER),
         c.relname
     FROM pg_class c, pg_index i
     WHERE c.oid = i.indexrelid
-    
     AND c.relname IN (
         SELECT
             relname
@@ -65,17 +58,17 @@ cur.execute("""INSERT INTO cascade.indices
                 FROM gt_name)
             AND pg_class.oid = pg_index.indrelid
             AND indisunique != 't'
-            AND indisprimary != 't' ))
-""")
+            AND indisprimary != 't' ))""")
 
-# Obtain OID of com_cas index
-cur.execute("SELECT * FROM cascade.indices WHERE idx_name = 'com_cas_geom_idx';")
-rows = cur.fetchone()
-rows = list(rows)
-oid = rows[0]
+cur.execute("""
+    SELECT * FROM cascade.indices
+    WHERE idx_name = 'com_cas_geom_idx';""")
+index = cur.fetchone()
+index = list(index)
+index = index[0]
 
-# geometry type of com_cas table
-cur.execute("""SELECT 
+cur.execute("""
+    SELECT 
         CASE 
             WHEN type = 'MULTIPOLYGON' THEN 'POLYGON'
             ELSE type
@@ -87,9 +80,9 @@ cur.execute("""SELECT
         ON idx_name = indexname
 	    WHERE idx_oid::integer = %s);
     """,
-            [oid])
+            [index])
 g_type = cur.fetchone()
-# srid of com_cas table
+
 cur.execute("""SELECT 
         srid
     FROM geometry_columns
@@ -99,21 +92,8 @@ cur.execute("""SELECT
         ON idx_name = indexname
 	    WHERE idx_oid::integer = %s);
     """,
-            [oid])
+            [index])
 g_srid = cur.fetchone()
-
-# cur.execute("""
-#     INSERT INTO cascade.com_cas
-#     SELECT c_code, geom
-#     FROM communes
-#     WHERE c_code = '12.066.01.03.'
-#     """)
-
-num_level = 1
-
-cur.execute("""
-    DROP TABLE IF EXISTS cascade.r_tree;
-    """)
 
 cur.execute("SELECT count(*) FROM communes;")
 count = cur.fetchone()
@@ -133,7 +113,7 @@ for i in range(count[0]):
         """,
                 [i])
 
-    cur.execute("SELECT gist_stat(%s);", [oid])
+    cur.execute("SELECT gist_stat(%s);", [index])
     stats = cur.fetchone()
 
     print(stats[0])
@@ -147,61 +127,65 @@ for i in range(count[0]):
         return(res)
 
     def expandB(lst):
-        # converting tuple to list [...]
         tmp = list(lst)
-        # l is a list that contains one element
-        # we splited the string on new line marks (\n)
         tmp = tmp[0].splitlines()
-        # l is now a list with 9 elements (len(l) = 9)
-        # this loop removes duplicate spaces in each element
         for e in range(len(tmp)):
             tmp[e] = " ".join(tmp[e].split())
-        # this function puts each element in its own list
-        # the result is a list of lists
         tmp = extractDigits(tmp)
 
         return(tmp)
 
-    l = expandB(stats)
+    stats = expandB(stats)
 
-    l = [sub.split(': ') for subl in l for sub in subl]
+    stats = [sub.split(': ') for subl in stats for sub in subl]
 
-    table_name = 'cascade.tree_'+str(i)
-    cur.execute("""DROP TABLE IF EXISTS %s;
-        """ % table_name)
-    cur.execute("""CREATE TABLE %s (geom geometry(%%s, %%s))
-        """ % table_name,
-                [g_type[0], g_srid[0]])
+    cur.execute(sql.SQL("""
+        DROP TABLE IF EXISTS {table};
+            """).format(
+        table=sql.Identifier('cascade.tree_'+str(i))))
 
-    cur.execute("""INSERT INTO %s 
-    SELECT replace(a::text, '2DF', '')::box2d::geometry(Polygon, %%s)
-    FROM (SELECT * FROM gist_print(%%s) as t(level int, valid bool, a box2df) WHERE level = 1) AS subq
-    """ % table_name,
-                [g_srid[0], oid])
+    cur.execute(sql.SQL("""
+        CREATE TABLE {table} (geom geometry ({type}, {srid}));
+            """).format(
+        table=sql.Identifier('cascade.tree_'+str(i)),
+        type=sql.Identifier(g_type[0]),
+        srid=sql.Identifier(g_srid[0])))
 
-    cur.execute("DROP TABLE IF EXISTS cascade.r_tree;")
-    cur.execute("""CREATE TABLE cascade.r_tree (
+    cur.execute(sql.SQL("""
+        INSERT INTO {table}
+        SELECT replace(a::text, '2DF', '')::box2d::geometry(Polygon, {srid})
+        FROM (SELECT * FROM gist_print({index}) as t(level int, valid bool, a box2df) WHERE level = 1) AS subq""").format(
+        table=sql.Identifier('cascade.tree_'+str(i)),
+        srid=sql.Identifier(g_srid[0]),
+        index=sql.Identifier(index)
+    ))
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS cascade.r_tree (
             geom geometry(%s, %s));
         """,
                 [g_type[0], g_srid[0]])
 
-    cur.execute("""
-    INSERT INTO cascade.r_tree 
-    SELECT replace(a::text, '2DF', '')::box2d::geometry(Polygon, %s)
-    FROM (SELECT * FROM gist_print(%s) as t(level int, valid bool, a box2df) WHERE level = 1) AS subq
-    """,
-                [g_srid[0], oid])
+    cur.execute("TRUNCATE TABLE cascade.r_tree")
 
-    # commiting our changes to the database
+    cur.execute(sql.SQL("""
+            INSERT INTO cascade.r_tree
+            SELECT replace(a::text, '2DF', '')::box2d::geometry(Polygon, {srid})
+            FROM (SELECT * FROM gist_print({index}) as t(level int, valid bool, a box2df) WHERE level = 1) AS subq""").format(
+        srid=sql.Identifier(g_srid[0]),
+        index=sql.Identifier(index)
+    ))
+
     conn.commit()
 
-    # ending transaction to be able to run VACUUM ANALYZE afterwards
     cur.execute("END TRANSACTION;")
-    # VACUUM command serves for updating statistics stored in postgres db
-    # that relates to nour r_tree when we rerun the python script for other relations
-    cur.execute("VACUUM ANALYZE %s;" % table_name)
+
+    cur.execute(sql.SQL("""
+        VACUUM ANALYZE {table};""").format(
+        table=sql.Identifier('cascade.tree_'+str(i))))
+
     cur.execute("VACUUM ANALYZE cascade.r_tree;")
-    # we notify qgis of the updates to display changes on the fly
+
     cur.execute("NOTIFY qgis, 'refresh qgis';")
 
 conn.commit()
