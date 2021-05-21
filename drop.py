cdd53b5999006492aa8012191699686a4807557f
salahelfarissi <salaheddineelfarissi@gmail.com>
2021-05-21 10:14:49 +0100

    ci

--- drop.py
+++ drop.py
@@ -27,19 +27,23 @@ cur.execute("""
     DROP TABLE IF EXISTS level_2.r_tree_l2;
     """)
 
+cur.execute("""
+    DROP TABLE IF EXISTS level_2.r_tree_l1;
+    """)
+
 cur.execute("SELECT count(*) FROM maroc.communes;")
 count = cur.fetchone()
 
 for i in range(1, count[0]+1):
     table_name = 'level_1.tree_l1_'+str(i)
     cur.execute("""
-        DROP TABLE IF EXISTS %s;
+        DROP TABLE IF EXISTS % s;
         """ % table_name)
 
 for i in range(1, count[0]+1):
     table_name = 'level_2.tree_l2_'+str(i)
     cur.execute("""
-        DROP TABLE IF EXISTS %s;
+        DROP TABLE IF EXISTS % s;
         """ % table_name)
 
 conn.commit()
