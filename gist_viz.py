2457c0528bc6a0db98aa9b94f5fcb8a6ee1d9b85
salahelfarissi <SalahEddineElFarissi@gmail.com>
2021-06-08 09:56:21 +0100

    indidces table created

--- gist_viz.py
+++ gist_viz.py
@@ -52,32 +52,8 @@ cur.execute("""
             """)
 
 cur.execute("""
-    WITH gt_name AS (
-        SELECT
-            f_table_name AS t_name
-        FROM geometry_columns
-    )
-    SELECT
-        CAST(c.oid AS INTEGER) as "OID",
-        c.relname as "INDEX"
-    FROM pg_class c, pg_index i
-    WHERE c.oid = i.indexrelid
-    AND c.relname IN (
-        SELECT
-            relname
-        FROM pg_class, pg_index
-        WHERE pg_class.oid = pg_index.indexrelid
-        AND pg_class.oid IN (
-            SELECT
-                indexrelid
-            FROM pg_index, pg_class
-            WHERE pg_class.relname IN (
-                SELECT t_name
-                FROM gt_name)
-            AND pg_class.oid = pg_index.indrelid
-            AND indisunique != 't'
-            AND indisprimary != 't' ));
-            """)
+    SELECT * FROM indices;
+    """)
 
 indices = cur.fetchall()
 indices = pd.Series(dict(indices))
