589ef0afa0a91d700be29b5c47af184e58091ebb
salahelfarissi <salaheddineelfarissi@gmail.com>
2021-06-11 19:36:57 +0100

    Added indices table creation

--- r_tree.py
+++ r_tree.py
@@ -24,6 +24,35 @@ cur.execute("""
     """)
 
 cur.execute("""
+    INSERT INTO indices
+    WITH gt_name AS (
+        SELECT
+            f_table_name AS t_name
+        FROM geometry_columns
+    )
+    SELECT
+        CAST(c.oid AS INTEGER) as "OID",
+        c.relname as "INDEX"
+    FROM pg_class c, pg_index i
+    WHERE c.oid = i.indexrelid
+    AND c.relname IN (
+        SELECT
+            relname
+        FROM pg_class, pg_index
+        WHERE pg_class.oid = pg_index.indexrelid
+        AND pg_class.oid IN (
+            SELECT
+                indexrelid
+            FROM pg_index, pg_class
+            WHERE pg_class.relname IN (
+                SELECT t_name
+                FROM gt_name)
+            AND pg_class.oid = pg_index.indrelid
+            AND indisunique != 't'
+            AND indisprimary != 't' ));
+            """)
+
+cur.execute("""
     SELECT * FROM indices();
             """)
 
