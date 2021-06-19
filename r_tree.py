82898441329c0a01e0b5b6c9a5fbfa0b53972dda
salahelfarissi <SalahEddineElFarissi@gmail.com>
2021-06-19 18:44:00 +0100

    Emoji exit for r_tree script

--- r_tree.py
+++ r_tree.py
@@ -5,36 +5,48 @@ from func import *  # user defined functions
 import pandas as pd
 
 # Use connect class to establish connection to PostgreSQL
-conn = connect("""
+conn = connect(
+    """
     host=192.168.1.106
     dbname=nyc
     user=postgres
-    """)
+    """
+)
 
 cur = conn.cursor()
 
 # You find a sql function to execute beforehand in queries folder
-cur.execute("""
+cur.execute(
+    """
     SELECT * FROM indices();
-            """)
+            """
+)
 
 indices = cur.fetchall()
 
 # Display a two column table with index and oid
 w1, w2 = field_width(indices)
 
-print(f'\n{"Index":>{w1}}{"OID":>{w2}}', '-'*(w1 + w2), sep='\n')
+print(f'\n{"Index":>{w1}}{"OID":>{w2}}', "-" * (w1 + w2), sep="\n")
 
 for oid, name in indices:
-    print(f'{name:>{w1}}{oid:>{w2}}')
+    print(f"{name:>{w1}}{oid:>{w2}}")
 
 # Ask the user which index to visualize
 try:
-    idx_oid = int(input("""
-        \nWhich GiST index do you want to visualize?\nOID → """))
+    idx_oid = int(
+        input(
+            """
+        \nWhich GiST index do you want to visualize?\nOID → """
+        )
+    )
 except ValueError:
-    idx_oid = int(input("""
-        \nYou must enter an integer value!\nOID → """))
+    idx_oid = int(
+        input(
+            """
+        \nYou must enter an integer value!\nOID → """
+        )
+    )
 
 # g_srid() function must be created beforehand (queries folder)
 cur.execute("SELECT g_srid(%s);", [idx_oid])
@@ -46,25 +58,31 @@ stat = pd.Series(unpack(cur.fetchone()))
 print(f"\nTree has a depth of {stat.Levels}.\n")
 level = int(input("Which level do you want to visualize?\nLevel → "))
 
-print(f'\n{58:c}{45:c}{41:c}\n')
+print("\n¯\_(ツ)_/¯\n")
 
-cur.execute("""
+cur.execute(
+    """
     DROP TABLE IF EXISTS r_tree;
-    """)
+    """
+)
 
-cur.execute("""
+cur.execute(
+    """
     CREATE TABLE r_tree (
         id serial primary key,
         geom geometry(POLYGON, %s));
     """,
-            [g_srid[0]])
+    [g_srid[0]],
+)
 
-cur.execute("""
+cur.execute(
+    """
     INSERT INTO r_tree (geom)
     SELECT st_setsrid(replace(a::text, '2DF', '')::box2d::geometry, %s)
     FROM (SELECT * FROM gist_print(%s) as t(level int, valid bool, a box2df) WHERE level = %s) AS subq
     """,
-            [g_srid[0], idx_oid, level])
+    [g_srid[0], idx_oid, level],
+)
 
 conn.commit()
 
